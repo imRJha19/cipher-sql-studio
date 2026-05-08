59f769f4fdabe0c1681761db99def22eec1a8cb1
imRJha19 <2023uce0041@iitjammu.ac.in>
2026-05-08 15:36:02 +0530

    Update routes logic

--- backend/src/routes/attempts.js
+++ backend/src/routes/attempts.js
@@ -7,7 +7,7 @@ const {
 const { protect } = require('../middleware/auth');
 
 const router = express.Router();
-
+# updating the logic for the route 
 router.get('/my', protect, getMyAttempts);
 router.get('/:assignmentId', protect, getAttemptByAssignment);
 router.patch('/:assignmentId/save', protect, saveQuery);
